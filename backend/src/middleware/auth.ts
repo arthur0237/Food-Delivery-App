34bffa996458c55b86ddf2825a96a69b0a807704
arthur0237 <tiwariashutosh0237@gmail.com>
2024-04-09 21:47:04 +0530

    Fix: Remove Audiance

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -13,6 +13,7 @@ declare global {
 }
 
 export const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: "RS256",
 });
